d7f5e4f79a38ed6f2b510a567b5095f764981958
Eduardo Araujo <eduardoaraujodasilva@protonmail.com>
2025-12-16 20:12:13 +0000

    feat: add functions and procedures to calculate and populate bombs around

--- game.c
+++ game.c
@@ -41,6 +41,56 @@ void drawBombs(int numberOfBombs){
   }
 }
 
+// Function that verifies if coordinates are valid or not: [0 - not valid] and [1 - valid]
+int coordinateIsValid(int r, int c){
+  if(r >= 0 && r < boardSize && c >= 0 && c < boardSize){
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+// Function that calculates and returns the number of bombs around `row` and `column`
+int calculateBombsAround(int r, int c){
+  /* Task: check the position immediately above, below, left and right
+    above = r - 1 and c
+    below = r + 1 and c
+    right = r and c + 1
+    left = r and c - 1
+    verify if the coordinate is valid
+  */
+
+  int quantity = 0;
+
+  // above
+  if(coordinateIsValid(r - 1, c) && game[r-1][c].isBomb){
+    quantity++;
+  }
+  // below
+  if(coordinateIsValid(r + 1, c) && game[r+1][c].isBomb){
+    quantity++;
+  }
+  // right
+  if(coordinateIsValid(r, c + 1) && game[r][c+1].isBomb){
+    quantity++;
+  }
+  // left
+  if(coordinateIsValid(r, c - 1) && game[r][c-1].isBomb){
+    quantity++;
+  }
+
+  return quantity;
+}
+
+// Procedure that populates the `bombsAround` field
+void populateBombsAround(){
+  for(row = 0; row < boardSize; row++){
+      for(column = 0; column < boardSize; column++){
+        game[row][column].bombsAround = calculateBombsAround(row, column);
+      }
+    }
+}
+
 int main(){
     initializeGame();
     drawBombs(10);
